c9425b8d9f1f46d6d4a32c8baf316cd6382ea9c6
JamesShi <jamesshi@oringnet.com>
2025-11-06 14:34:50 +0800

    chore: update version to 1.0.5 in package.json; refactor joinZero function to improve parameter handling in joinZero.ts

--- src/joinZero.ts
+++ src/joinZero.ts
@@ -1,7 +1,7 @@
-// export function joinZero(value: number): string {
-//   return (Array(8).join('0') + value.toString(10)).slice(8 * -1);
-// }
-
-export function joinZero(value: number, length: number): string {
-  return (Array(length).join("0") + value.toString(10)).slice(length * -1);
+export function joinZero(value: number): string {
+  return (Array(8).join('0') + value.toString(10)).slice(8 * -1);
 }
+
+// export function joinZero(value: number, length: number): string {
+//   return (Array(length).join("0") + value.toString(10)).slice(length * -1);
+// }
