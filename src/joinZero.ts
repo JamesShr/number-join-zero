0545dfd3645012a01cbeecd7879869a444567fbe
JamesShi <jamesshi@oringnet.com>
2025-11-06 14:18:55 +0800

    feat(1.0.4):edit fun

--- src/joinZero.ts
+++ src/joinZero.ts
@@ -2,8 +2,6 @@
 //   return (Array(8).join('0') + value.toString(10)).slice(8 * -1);
 // }
 
-export function joinZero(value: number, length?: number): string {
-  return (Array(length || 8).join('0') + value.toString(10)).slice(
-    length || 8 * -1,
-  );
+export function joinZero(value: number, length: number): string {
+  return (Array(length).join("0") + value.toString(10)).slice(length * -1);
 }
